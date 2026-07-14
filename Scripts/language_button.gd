4f931894c5b644ab7ebb16b98e09ae6059346721
Kyou K <koowky@gmail.com>
2026-07-14 20:06:06 +0900

    language select start, more mouseover highlighting

--- Scripts/language_button.gd
+++ Scripts/language_button.gd
@@ -1,8 +1,72 @@
 extends Button
 
 @export var parent_control : Control;
+@export_group("Textures", "texture_")
+@export var texture_underline : Control;
+@export var texture_hover : Control;
+@export var texture_select_left : Control;
+@export var texture_select_right: Control;
+
+@export var vertical_offset : int = -8;
+@export var horizonal_offset : int = 16;
+## 1:x ratio for how wide the underline should be
+@export_range(0, 1.0, 0.05) var underline_width : float = 0.8;
+var is_focused : bool = false;
+
+func position_underline() -> void:
+    # LMAO: im intelligent really y is definitely used for width /s
+    var text_width : float = self.size.x;
+    texture_underline.size = Vector2(text_width * underline_width, texture_underline.size.y);
+    texture_underline.position = Vector2(text_width * ((1 - underline_width)/2), texture_underline.position.y);
+
+func _on_focus_entered() -> void:
+    texture_underline.visible = true;
+
+func _on_focus_exited() -> void:
+    texture_underline.visible = false;
+
+func _on_mouse_entered() -> void:
+    # TODO: just handle this using a new hover texture jfc
+    Statics.debug_log("i am hovered {0}".format([self.name]));
+    texture_underline.visible = true;
+
+func _on_mouse_exited() -> void:
+    # self.theme.set_constant("outline_size", "nt", 0);
+    # self.theme.set_color("text_outline", "nt", Color(0, 0, 0, 0));
+    texture_underline.visible = false;
+
+func _on_toggle(is_active : bool) -> void:
+    texture_select_left.visible = is_active;
+    texture_select_right.visible = is_active;
+    if (is_active): parent_control.public_set_active_button(self);
+
+func _on_child_set_active(b : Button) -> void:
+    Statics.debug_log("b {0} is me? {1}".format([b.name, self.name]))
+    texture_select_left.visible = (b == self);
+    texture_select_right.visible = (b == self);
 
 func _ready() -> void:
     if (parent_control == null):
         # naive search
         parent_control = self.get_parent();
+
+    assert(texture_underline != null, "{0} must have underline texture".format([self.name]));
+    position_underline();
+    texture_underline.visible = false;
+    assert(texture_select_left != null, "{0} must have left texture".format([self.name]));
+    texture_select_left.position = Vector2(-(texture_select_left.size.x * texture_select_left.scale.x) - horizonal_offset, vertical_icon_alignment);
+    texture_select_left.visible = false;
+    assert(texture_select_right != null, "{0} must have right texture".format([self.name]));
+    texture_select_right.position = Vector2(self.size.x + horizonal_offset, vertical_icon_alignment);
+    texture_select_right.visible = false;
+    assert(texture_hover != null, "{0} must have hover texture".format([self.name]));
+    #TODO: position texture
+
+    texture_hover.visible = false;
+
+    self.focus_entered.connect(_on_focus_entered);
+    self.focus_exited.connect(_on_focus_exited);
+    self.mouse_entered.connect(_on_mouse_entered);
+    self.mouse_exited.connect(_on_mouse_exited);
+    self.toggled.connect(_on_toggle);
+    parent_control.child_set_active.connect(_on_child_set_active)
--- Scripts/language_toggle.gd
+++ Scripts/language_toggle.gd
@@ -5,6 +5,13 @@ extends Control
 @export var en_button : Button;
 @export var focus_background : Control;
 var is_child_button_focused : bool = false;
+var active_button : Button;
+
+signal child_set_active(b : Button);
+
+func public_set_active_button(b : Button) -> void:
+    active_button = b;
+    child_set_active.emit(b);
 
 func _on_mouse_entered() -> void:
     focus_background.visible = true;
@@ -13,6 +20,18 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
     focus_background.visible = false;
     button_flow_handler.public_set_unhover(self);
+
+func _on_focus_entered() -> void:
+    if (ja_button.button_pressed):
+        ja_button.grab_focus.call_deferred();
+    else:
+        en_button.grab_focus.call_deferred();
+    focus_background.visible = true;
+
+func _on_child_button_focus_exited() -> void:
+    if (ja_button.has_focus() or en_button.has_focus()):
+        return;
+    focus_background.visible = false;
 
 func _on_no_hover(c : Control) -> void:
     if (c == self or c == ja_button or c == en_button):
@@ -35,6 +54,7 @@ func _ready() -> void:
             parent = parent.get_parent();
         button_flow_handler = parent;
 
+    # TODO: define this based on language
     var radio_group : ButtonGroup = ButtonGroup.new();
     radio_group.allow_unpress = false;
     ja_button.toggle_mode = true;
@@ -42,9 +62,14 @@ func _ready() -> void:
     ja_button.button_group = radio_group;
     en_button.toggle_mode = true;
     en_button.button_group = radio_group;
+    child_set_active.emit(ja_button);
+    active_button = ja_button;
 
     self.mouse_entered.connect(_on_mouse_entered);
     self.mouse_exited.connect(_on_mouse_exited);
+    self.focus_entered.connect(_on_focus_entered);
+    ja_button.focus_exited.connect(_on_child_button_focus_exited);
+    en_button.focus_exited.connect(_on_child_button_focus_exited);
     button_flow_handler.hover_removed.connect(_on_no_hover);
 
     focus_background.visible = false;
